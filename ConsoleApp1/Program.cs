980e11a9ce7154297b18b3e327ff00e30abd00b9
amos.svenns <amos.svenns@gmail.com>
2026-09-22 13:27:12 +0200

    efrdgfh

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,109 +1,113 @@
 ﻿
 //bullet1 är live
+using Microsoft.VisualBasic;
+
 bool blank = true;
 
 bool dintur = true;
 
 bool återstartbool = false;
 
+int winst = 0;
+
+int förlust = 0;
+
+List<Action> kulor = new List<Action>()
+    {
+        bullet1,
+        bullet2
+    };
+
 void bullet1()
 {
     blank = false;
-    Console.WriteLine("panng");
+
 }
 
 void bullet2()
 {
-    Console.WriteLine("CLICK");
-}
-/*
-void bullet3()
-{ 
-    Console.WriteLine("CLICK");
+    blank = true;
 }
 
-void bullet4()
-{
-    Console.WriteLine("AAAAHHHHHHHHH CLICK");
-}
-
-void bullet5()
-{ 
-    Console.WriteLine("CLICK");
-}
-
-void bullet6()
-{ 
-    Console.WriteLine("CLICK");
-}
-
-void bullet7()
-{ 
-    Console.WriteLine("CLICK");
-}
-
-void bullet8()
-{
-    Console.WriteLine("CLICK");
-}
-*/
 void återstart()
 {
-    Console.Clear();
     Console.WriteLine("tryck r för återstart");
     återstartbool = true;
 
 }
 
+void kör()
+{
+    Console.ReadLine();
+}
+void start()
+{
+    Console.Clear();
+    Console.WriteLine("du kör nu rysk rollet tryck f och enter för att skjuta den du ser framfördig är din motståndare");
+    Console.WriteLine($"winst {winst} förlust {förlust}");
+    blank = true;
+    återstartbool = false;
+    kulor.Clear();
+    for (int i = 0; i < 7; i++)
+    {
+        kulor.Add(bullet2);
+    }
+    kulor.Add(bullet1);
+}
 
 Console.WriteLine("du kör nu rysk rollet tryck f och enter för att skjuta den du ser framfördig är din motståndare");
 while (true)
 {
 
+    kör();
 
+ 
 
-
-    List<Action> funktioner = new List<Action>()
-    {
-        bullet1
-    };
-
-    for (int i = 0; i < 7; i++)
-    {
-        funktioner.Add(bullet2);
-    }
     Random slump = new Random();
-    int index = slump.Next(funktioner.Count);
+    int index = slump.Next(kulor.Count);
 
-    funktioner[index]();
+    kulor[index]();
+    kulor.RemoveAt(index);
 
-    if (dintur == true && blank == false)
+
+    if (dintur == true && blank == true)
     {
+        Console.WriteLine("click¤");
         Console.WriteLine("du klämmer avtryckaren i med hög puls och hör ett klick");
         dintur = false;
     }
     else if (dintur == false && blank == true)
     {
+        Console.WriteLine("click¤");
         Console.WriteLine("din fiende trycker avtryckaren och hör ett klick ");
         dintur = true;
     }
 
-    if (dintur == true && blank == false)
+    else if (dintur == true && blank == false)
     {
+        Console.WriteLine("PANG!");
         Console.WriteLine("du hör ett högt skott och ser ett ljus och så långsamt börjar du falla neråt och ser din kropp framlutad utan liv i ögonen och då inser du vem du spelade med, det var djävulen");
         återstart();
+        blank = true;
+        dintur = false;
+        förlust += 1;
     }
     else if (dintur == false && blank == false)
     {
+        Console.WriteLine("PANG!");
         Console.WriteLine("du hör ett högt pang och så ser du din fiende falla framot och så börjar du långsamt stiga uppot och då inser du att du van mot djävulen i rysk rolet");
         återstart();
-    }
-       string återstartstring = Console.ReadLine;
+        dintur = true;
+        blank = true;
+        winst += 1;
     
+    }
 
-    if (återstartbool == true && återstartstring == "r")
+   string återStartstring = Console.ReadLine();
+    återStartstring = återStartstring.ToLower();
+    if (återstartbool == true && återStartstring == "r")
     {
-
+        start();
     }
 
 }
